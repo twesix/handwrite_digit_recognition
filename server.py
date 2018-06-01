63cfb924acd928c364659b0580738d04097e269a
twesix <278227739@qq.com>
2018-06-01 13:09:27 +0800

    前后端交互完成，但是网页不知道为啥乱码了

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request, make_response
 import os
+import json
 # from model import recognize
 
 os.environ['FLASK_DEBUG'] = str(1)
@@ -11,19 +12,23 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def root():
 
+    resp = None
+
     if request.method == 'POST':
-        print(request.get_json())
-        print(request.get_data())
+        array = json.loads(request.get_data().decode('utf-8'))['array']
+        print(array[0])
 
         response_text = 'OK'
         resp = make_response(response_text)
-        resp.headers['access-control-allow-origin'] = '*'
-        return resp
+
     if request.method == 'GET':
         with open('index.html') as f:
-            index_page = f.read()
-        return index_page
+            index_page = f.read().encode('utf-8')
+        resp = make_response(index_page)
+
+    resp.headers['access-control-allow-origin'] = '*'
+    return resp
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
